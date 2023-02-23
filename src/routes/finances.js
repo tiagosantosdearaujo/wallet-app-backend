87aa31ceab3b4767091fe6eb3fa716076e265deb
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-02-23 11:39:06 -0800

    feat: delete finance item implemented

--- src/routes/finances.js
+++ src/routes/finances.js
@@ -56,4 +56,49 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.headers;
+
+    if (email.lenght < 5 || !email.includes("@")) {
+      return res.status(400).json({ error: "Email is invalid." });
+    }
+
+    if (!id) {
+      return res.status(400).json({ error: "Id is mandatory" });
+    }
+
+    const userQuery = await db.query(usersQueries.findByEmail(email));
+    if (!userQuery.rows[0]) {
+      return res.status(403).json({ error: "User already exists" });
+    }
+
+    const findFinanceText = "SELECT * FROM finances WHERE id=$1";
+    const findFinanceValue = [Number(id)];
+    const financeItemQuery = await db.query(findFinanceText, findFinanceValue);
+
+    if (!financeItemQuery.rows[0]) {
+      return res.status(400).json({ error: "Finance row not found" });
+    }
+
+    if (financeItemQuery.rows[0].user_id !== userQuery.rows[0].id) {
+      return res
+        .status(401)
+        .json({ error: "Finances row does not belong to user" });
+    }
+
+    const text = "DELETE FROM finances WHERE id=$1 RETURNING *";
+    const values = [Number(id)];
+    const deleteResponse = await db.query(text, values);
+
+    if (!deleteResponse.rows[0]) {
+      return res.status(400).json({ error: "Finances row not deleted" });
+    }
+    return res.status(200).json(deleteResponse.rows[0]);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 module.exports = router;
