56180fca42b96f1c7fd3b9f963ae4506ae2fc72e
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-02-22 18:40:34 -0800

    create routes for categories, mainly post

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
-const { response } = require("express");
 const express = require("express");
 const db = require("./db");
+const routesCategories = require("./routes/categories");
 
 const app = express();
+app.use(express.json());
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/categories", (req, res) => {
-  db.query("SELECT * FROM categories", (error, response) => {
-    if (error) {
-      return res.status(500).json(error);
-    }
-
-    return res.status(200).json(response.rows);
-  });
-});
+app.use("/categories", routesCategories);
 
 app.listen(port, () => {
   db.connect()
---
+++ src/routes/categories.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM categories", (error, response) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.status(200).json(response.rows);
+  });
+});
+
+router.post("/", (req, res) => {
+  const { name } = req.body;
+
+  if (name.lenght < 3) {
+    return res
+      .status(400)
+      .json({ error: "Name should have more than 3 characters" });
+  }
+
+  const text = "INSERT INTO categories(name) VALUES($1) RETURNING *";
+  const values = [name];
+
+  db.query(text, values, (error, response) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    return res.status(200).json(response.rows);
+  });
+});
+
+module.exports = router;
