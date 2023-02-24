19cc079f092f80225bd5ce339dd928ad36dd2940
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-02-23 18:25:52 -0800

    feat: add DB url config for deploy

--- src/db.js
+++ src/db.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DB_URL } = process.env;
 
-const db = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: Number(DB_PORT),
-});
+const db = new Pool(
+  DB_URL
+    ? {
+        connectionString: DB_URL,
+      }
+    : {
+        user: DB_USER,
+        host: DB_HOST,
+        database: DB_NAME,
+        password: DB_PASSWORD,
+        port: Number(DB_PORT),
+      }
+);
 
 module.exports = db;
